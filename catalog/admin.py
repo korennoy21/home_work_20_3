a1aa59448931a17655cf41fa0b144c0fc8c62efb
Александр <aleksandr@MacBook-Pro-Aleksandr.local>
2024-08-24 02:51:47 +0300

    Initial commit

---
+++ catalog/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Product, Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
+
+
+admin.site.register(Category)
