7a6048b637629ec419627bdcf7ac9842b91ca9fb
aleksandr <korennoy21@gmail.com>
2024-08-26 03:17:53 +0300

    urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,20 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
+from catalog import views
 from catalog.apps import CatalogConfig
-from catalog.views import contact, home
+from .views import IndexView, ContactView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-                  path('', home, name='home'),
-                  path('contact/', contact, name='contact'),
+                  path('blogs/', BlogListView.as_view(), name='blog_list'),
+                  path('blogs/<slug:slug>/', BlogDetailView.as_view(), name='blog_detail'),
+                  path('blogs/create/', BlogCreateView.as_view(), name='blog_create'),
+                  path('blogs/<slug:slug>/update/', BlogUpdateView.as_view(), name='blog_update'),
+                  path('blogs/<slug:slug>/delete/', BlogDeleteView.as_view(), name='blog_delete'),
+                  path('', IndexView.as_view(), name='index'),
+                  path('contact/', ContactView.as_view(), name='contact'),
+                  path('product/<int:pk>/', views.product_detail, name='product_detail')
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
