69f61cd1d4522ac4bf4ac6d09da70b067e67f5f6
aleksandr <korennoy21@gmail.com>
2024-08-26 03:18:06 +0300

    models

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Category(models.Model):
@@ -28,3 +29,21 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200, unique=True, blank=True)
+    content = models.TextField()
+    preview_image = models.ImageField(upload_to='blog_previews/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(default=False)
+    views_count = models.PositiveIntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
