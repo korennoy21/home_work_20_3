1a9c1dcb56397ae4c88081ba644a7485611dda7d
aleksandr <korennoy21@gmail.com>
2024-08-26 06:14:29 +0300

    c

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,27 @@
 from django.db import models
+from django.utils.text import slugify
 
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    slug = models.CharField(max_length=200, unique=True, verbose_name='Slug')
+    content = models.TextField(verbose_name='Содержимое')
+    preview_image = models.ImageField(upload_to='blog_previews/', verbose_name='Превью')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    is_published = models.BooleanField(default=False, verbose_name='Признак публикации')
+    views = models.IntegerField(default=0, verbose_name='Количество просмотров')
+
+    class Meta:
+        verbose_name = 'Блоговая запись'
+        verbose_name_plural = 'Блоговые записи'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from slugify import slugify
-from .models import Product, Category, People
+from .models import Product, Category, People, BlogPost
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import BlogPost
 from django.views.generic import TemplateView
 
-
 class MyBaseFooter:
-    """Класс для выноса переопределения функции чтобы в футере была инфа динамически
-       не придумал как по другому это сделать класс сам по себе ничего не делает
-       просто существует для наследования чтобы пере-определить этот метод у других классов"""
+    """Класс для выноса переопределения функции, чтобы в футере была инфа динамически.
+       Класс сам по себе ничего не делает, просто существует для наследования,
+       чтобы переопределить этот метод у других классов."""
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -21,25 +19,25 @@ class MyBaseFooter:
 
 class ContactsView(MyBaseFooter, CreateView):
     """Отображение странички контактов и
-    формы сбора контактов от пользователя с последующим сохранением в бд"""
+    формы сбора контактов от пользователя с последующим сохранением в БД."""
     model = People
     fields = ('name', 'phone_number', 'message')
     success_url = reverse_lazy('catalog:contacts')
 
 
 class IndexView(MyBaseFooter, ListView):
-    """Главная каталога"""
+    """Главная страница каталога."""
     model = Product
     template_name = 'catalog/index.html'
 
 
 class ProductView(MyBaseFooter, DetailView):
-    """Один товар"""
+    """Детальный просмотр одного товара."""
     model = Product
 
 
 class CategoryView(MyBaseFooter, DetailView):
-    """Одна категория со всеми товарами в ней реализованно через класс MyBaseFooter"""
+    """Просмотр одной категории со всеми товарами в ней, реализованно через класс MyBaseFooter."""
     model = Category
 
 
@@ -49,5 +47,16 @@ def product_detail(request, pk):
     return render(request, 'catalog/product_detail.html', context)  # Рендерим шаблон и передаем контекст
 
 
-class BlogListView:
-    pass
+class BlogListView(MyBaseFooter, ListView):
+    """Отображение списка всех блоговых записей."""
+    model = BlogPost
+    template_name = 'catalog/blog_list.html'
+    context_object_name = 'blog_posts'
+
+    def get_queryset(self):
+        """Возвращает опубликованные записи, отсортированные по дате создания."""
+        return BlogPost.objects.filter(is_published=True).order_by('-created_at')
+
+
+
+
