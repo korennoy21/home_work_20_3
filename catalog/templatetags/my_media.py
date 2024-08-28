0a3ef641ff8cadbe9e94542b145e8cc3337cdb5d
aleksandr <korennoy21@gmail.com>
2024-08-28 04:57:45 +0300

    правки

--- catalog/templatetags/my_media.py
+++ catalog/templatetags/my_media.py
@@ -3,8 +3,8 @@ from django import template
 register = template.Library()
 
 
-@register.filter(name='mymedia')
-def mymedia(data):
+@register.filter(name='my_media')
+def my_media(data):
     if data:
         return f'/media/{data}'
     return '#'
